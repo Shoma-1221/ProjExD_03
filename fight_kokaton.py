3483f0ad2fc7e7b50913427ab6f1287018657d12
SHOMA <c0b22070e1@edu.teu.ac.jp>
2023-06-27 16:06:58 +0900

    練習合体

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -150,7 +150,7 @@ def main():
     beam = None
 
     clock = pg.time.Clock()
-    tmr = 0
+    tmr = 0 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
